d0d418eec39ac7ab89784ce83806cb574c84fb17
Jason Martinez <ddritg2098@gmail.com>
2022-02-07 19:35:50 -0600

    Basic bug spawner and scene made
    Simple placeholder bug spawner (using cubes) made along with a Testing scene.  I may consider making the placeholder bug objects destroyable after a couple of seconds.

---
+++ BugGame/Assets/BugSpawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugSpawner : MonoBehaviour
+{
+    public GameObject bug;
+    private int xPosition;
+    private int zPosition;
+    private int bugsSpawned;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(spawnBug());
+    }
+
+    IEnumerator spawnBug()
+    {
+        while(bugsSpawned < 15)
+        {
+            xPosition = Random.Range(100, 890);
+            zPosition = Random.Range(100, 830);
+            Instantiate(bug, new Vector3(xPosition, 25, zPosition), Quaternion.identity);
+            yield return new WaitForSeconds(5);
+            bugsSpawned += 1;
+        }
+    }
+}
