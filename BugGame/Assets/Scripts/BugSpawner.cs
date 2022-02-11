678226e2a7913f581936baf7266d37ead241bb86
Jason Martinez <ddritg2098@gmail.com>
2022-02-10 21:09:54 -0600

    Added comment related to spawner, no changes.

--- BugGame/Assets/Scripts/BugSpawner.cs
+++ BugGame/Assets/Scripts/BugSpawner.cs
@@ -1,3 +1,11 @@
+// If someone can help me on how I can make a decent spawning system, that would be appreciated.
+// I haven't used Unity and coded in C# in 5 years, so it would be appreciated if someone can give me
+// a headstart to go back to the level where I was back in 2017, because at this moment I'm just lost.
+// - Jason Martinez
+
+// This is a placeholder spawner, do not use this. I plan on rewritting it soon.
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
