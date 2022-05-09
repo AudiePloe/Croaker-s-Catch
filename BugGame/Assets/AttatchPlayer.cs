eda9b1109925e6c49365ef5d1b0f9bfe82f76af4
SheriffMenty <98922228+SheriffMenty@users.noreply.github.com>
2022-05-08 21:05:41 -0500

    Added audio and moving platform

---
+++ BugGame/Assets/AttatchPlayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttatchPlayer : MonoBehaviour
+{
+    public GameObject Player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == Player)
+        {
+            Player.transform.parent = transform;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == Player)
+        {
+            Player.transform.parent = null;
+        }
+    }
+
+}
