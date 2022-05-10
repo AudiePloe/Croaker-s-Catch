928b4c20a3c3d7c6e5170d7b51d22fde62c5bc66
humberto <htg702001@hotmail.com>
2022-05-09 22:03:13 -0500

    BugShootParticleEffect

---
+++ BugGame/Assets/Scripts/BugCatchParticleEffect/EffectObject.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectObject : MonoBehaviour{
+
+    public float time;
+
+    void Start ()
+    {
+        Destroy(gameObject, time);
+    }
+
+}
