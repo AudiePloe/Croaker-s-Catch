4f74b3e0f5a7689298939535dc13da86f0e487cc
Audie Ploe <aploe@student.alamo.edu>
2022-05-10 15:05:52 -0500

    Added Info change on bug pages

--- BugGame/Assets/FlipSprite.cs
+++ BugGame/Assets/FlipSprite.cs
@@ -8,17 +8,28 @@ public class FlipSprite : MonoBehaviour
     public GameObject BlackPic;
     public GameObject ColorPic;
 
+    public GameObject NoInfo;
+    public GameObject FilledInfo;
+
 
     public void SwapPic()
     {
         BlackPic.SetActive(false);
         ColorPic.SetActive(true);
+
+        NoInfo.SetActive(false);
+        FilledInfo.SetActive(true);
+
     }
 
     public void resetImages()
     {
         BlackPic.SetActive(true);
         ColorPic.SetActive(false);
+
+        NoInfo.SetActive(true);
+        FilledInfo.SetActive(false);
+
     }
 
 }
